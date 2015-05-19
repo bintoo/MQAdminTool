4392eda37a444e2b192c9e811dfb83aedaccfc97
jzhou <jzhou@KVM832-PC00YM3W>
2015-05-20 10:09:37 +1200

    Help page improved

--- src/UI/Dialogs/HelpDialog.java
+++ src/UI/Dialogs/HelpDialog.java
@@ -5,6 +5,13 @@
  */
 package UI.Dialogs;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
 /**
  *
  * @author jzhou
@@ -17,8 +24,49 @@ public class HelpDialog extends javax.swing.JDialog {
     public HelpDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        this.ContentTextArea.setWrapStyleWord(true);
-        this.ContentTextArea.setLineWrap(true);
+        String text = "<p>This project is developed by Jianbin Zhou for study purpose.</p>" +
+                        "<p>Please report any bug or suggestion to my email address " +
+                        "<a href='mailto:curtiszhou1981@gmail.com'>curtiszhou1981@gmail.com</a></p>"+
+                        "<p>Icons from Icon8 <a href=\"https://icons8.com/\">https://icons8.com/</a>" +
+                        "<p>Known issues :<ul>\n" +
+                        "   <li>Create/update MQ objects not working correctly on mainframe.</li>" +
+                        "  <li>Update message feature not working correctly when the queue has more then 1 message with same id.</li>" +
+                        " <li>Currently not support local connection to queue manager</li>" +
+                            "</ul> </p>";
+        
+                        
+        this.TextPane.setContentType("text/html");
+        this.TextPane.setText(text);
+        this.TextPane.setEditable(false);
+        this.TextPane.addHyperlinkListener(new HyperlinkListener() {
+
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                    if(e.getURL().toString().startsWith("http")){
+                        try {
+                            try {
+                                Desktop.getDesktop().browse(new URI(e.getURL().toString()));
+                            } catch (IOException ex) {
+                            }
+                        }
+                        catch (URISyntaxException ex) {
+
+                        }                        
+                    }
+                else{
+                        try {
+                            Desktop.getDesktop().mail(new URI(e.getURL() + ""));
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        } catch (URISyntaxException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+        });
     }
 
     /**
@@ -31,8 +79,8 @@ public class HelpDialog extends javax.swing.JDialog {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        ContentTextArea = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        TextPane = new javax.swing.JTextPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("About");
@@ -40,16 +88,10 @@ public class HelpDialog extends javax.swing.JDialog {
         setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
-        jLabel1.setText("MQ Admin Tool beta (build )");
+        jLabel1.setText("       MQ Admin Tool beta (build 8a5f4ab )");
         jLabel1.setPreferredSize(new java.awt.Dimension(300, 50));
 
-        ContentTextArea.setEditable(false);
-        ContentTextArea.setColumns(20);
-        ContentTextArea.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        ContentTextArea.setRows(5);
-        ContentTextArea.setText("This project is developed by Jianbin Zhou for study purpose.\n\nPlease report any bug or recommadation to my email address\n\njzhou@nz1.ibm.com. ");
-        ContentTextArea.setPreferredSize(new java.awt.Dimension(380, 210));
-        jScrollPane1.setViewportView(ContentTextArea);
+        jScrollPane2.setViewportView(TextPane);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -58,7 +100,7 @@ public class HelpDialog extends javax.swing.JDialog {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1)
+                    .addComponent(jScrollPane2)
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE))
                 .addContainerGap())
         );
@@ -67,8 +109,8 @@ public class HelpDialog extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 222, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -80,8 +122,8 @@ public class HelpDialog extends javax.swing.JDialog {
      */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextArea ContentTextArea;
+    private javax.swing.JTextPane TextPane;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
 }
